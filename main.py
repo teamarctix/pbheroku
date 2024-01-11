a95bb1e2ef6b3a2e4601d7faae65578e06f2e007
ITZ ARCTIX <144344981+teamarctix@users.noreply.github.com>
2024-01-11 10:58:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ def ytdlpp(link):
     print(f"Download Completed {link}")
     time.sleep(120)
     up["ytdl"] = True
-
 async def main():
     async with app:
         for link in links:
@@ -42,11 +41,14 @@ async def main():
                     for filename in os.listdir():
                         if filename.endswith(".mp4"):
                             try:
-                                video = await app.send_video(-1002034630043, video=filename, caption=filename.replace(".mp4", ""), supports_streaming=True)
+                                command = f'vcsi "{filename}" -g 2x2 --metadata-position hidden -o "{filename.replace(".mp4",".png")}"'
+                                result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                                await app.edit_message_text(-1002034630043, sts.id, f"Uploaded Videos:{up['Total']}\nUploading {filename}")
+                                video = await app.send_video(-1002034630043, video=filename, caption=filename.replace(".mp4",""), thumb=filename.replace(".mp4",".png"), supports_streaming=True, progress=progress)
                                 up['Total'] += 1
                                 await app.edit_message_text(-1002034630043, sts.id, f"Uploaded Videos:{up['Total']}\nUploaded {filename}")
                                 try:
-                                    command = f'rm -f "{filename}"' if os.name != 'nt' else f'del /Q "{filename}"'
+                                    command = f'rm -f "{filename}" "{filename.replace(".mp4", ".png")}"' if os.name != 'nt' else f'del /Q "{filename}" "{filename.replace(".mp4", ".png")}"'
                                     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                                     await app.edit_message_text(-1002034630043, sts.id, f"Uploaded Videos:{up['Total']}\nCleared {filename}")
                                 except:
